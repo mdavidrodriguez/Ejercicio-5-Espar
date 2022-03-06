5ecd81069dd4afee58df9b5657f9c4d27a3cd89f
mdrod <mdrod@DESKTOP-H6TTJ8Q>
2022-03-06 14:49:10 -0500

    Agregar archivos de proyecto.

---
+++ Espar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Espar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Saber si un numero es par o impar");
+            Console.WriteLine("Digite un numero: ");
+            int numero =int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Es Par");
+            }
+            else
+            {
+                Console.WriteLine("Es impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
